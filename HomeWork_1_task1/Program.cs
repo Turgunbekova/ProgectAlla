0f92cad004787a6a4726334cc8f1589b528d0572
Alla <albina7333331@gmail.com>
2023-09-19 20:47:25 +0300

    Homework1

---
+++ HomeWork_1_task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+bool Chto0 = a > b;
+bool Chto1 = a == b;
+bool Chto2 = a < b;
+
+if(Chto0)
+{
+    Console.WriteLine("max=a  min=b");
+}
+else if (Chto1)
+{
+    Console.WriteLine("a=b");
+}
+else
+{
+    Console.WriteLine("max=b  min=a");
+}
+
+
