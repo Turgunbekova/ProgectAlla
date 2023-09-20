0ce6fa8f6f0fc6f57a62d99f6c282b20b462d954
Alla <albina7333331@gmail.com>
2023-09-20 19:39:19 +0300

    Task2

---
+++ HomeWork_1_task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2){
+ Console.Write($"max = min = {num1}");
+ }
+ else if (num1 < num2){
+ Console.Write($"max = {num2}, min = {num1}");
+ }
+ else{
+ Console.Write($"max = {num1}, min = {num2}");
+ }
+ Console.WriteLine();
